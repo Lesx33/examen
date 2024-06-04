90de83b52826ef17893100661ff88895ffd4fba6
Lesx33 <mokiks979@gmail.com>
2024-06-04 11:43:50 -0600

    Agregando service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoggingService } from './services/exa.service';
+
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,9 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'exa1';
-}
+  constructor(private loggingService: LoggingService) {
+    this.loggingService.impresion()
+}}
